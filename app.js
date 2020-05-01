f41471ea63c2a8fd8568030ba6d28940e5e1dfee
Anthony <anthonychansoon@gmail.com>
2020-05-01 00:53:01 -0700

    https

--- app.js
+++ app.js
@@ -13,13 +13,49 @@ app.get("/", (req, res) => {
     res.sendFile(__dirname + "/signup.html");
 })
 
-app.post("/", function(req, res) {
-    console.log(req.body);
+app.post("/", function (req, res) {
+    //console.log(req.body);
     var fn = req.body.FN;
     var ln = req.body.LN;
     var eMail = req.body.EMAIL
+
+    var data = {
+        members: [{
+            email_address: eMail,
+            status: "subscribed",
+            merge_fields: {
+                FNAME: fn,
+                LNAME: ln
+            }
+        }]
+    }
+
+    var jsonData = JSON.stringify(data);
+
+
+    const url = "https://us8.api.mailchimp.com/3.0/lists/bb449cf895";
+
+    const options = {
+        method: "POST",
+        auth: "helloworld:98e67653a037e217c3442dfcdb6634b7-us8"
+    }
+
+    const request = https.request(url, options, function (response) {
+        response.on("data", function (data) {
+            console.log(JSON.parse(data));
+        }
+
+        )
+    })
+
+    request.write(jsonData);
+    request.end();
 })
 
 app.listen(3000, () => {
     console.log("localhost connected port 3000");
 })
+
+//apikey for mailchimp 98e67653a037e217c3442dfcdb6634b7-us8
+
+//listid bb449cf895
